bcb69eb10e28967a63bbfc13f0c0c7c1dce647e0
Denis Koshenkov <dkoshenkov@gmail.com>
2026-01-12 18:20:48 +0100

    Call create in test group setup

--- test/ring_buf.cpp
+++ test/ring_buf.cpp
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include "CppUTest/TestHarness.h"
 #include "CppUTestExt/MockSupport.h"
 
@@ -9,22 +11,38 @@
 /* To return from mock_ring_buf_get_inst_buf */
 static struct RingBufStruct inst_buf;
 
-TEST_GROUP(RingBuf){};
+RingBuf ring_buf;
+RingBufInitCfg init_cfg;
+
+void *get_inst_buf_user_data = (void *)0x20;
+
+static void populate_default_init_cfg(RingBufInitCfg *const cfg)
+{
+    cfg->get_inst_buf = mock_ring_buf_get_inst_buf;
+    cfg->get_inst_buf_user_data = get_inst_buf_user_data;
+}
+
+// clang-format off
+TEST_GROUP(RingBuf){
+    void setup() {
+        ring_buf = NULL;
+        memset(&init_cfg, 0, sizeof(RingBufInitCfg));
+        memset(&inst_buf, 0, sizeof(struct RingBufStruct));
+
+        /* Every test should call ring_buf_create at the beginning, which will call this mock */
+        mock()
+            .expectOneCall("mock_ring_buf_get_inst_buf")
+            .withParameter("user_data", get_inst_buf_user_data)
+            .andReturnValue((void *)&inst_buf);
+
+        populate_default_init_cfg(&init_cfg);
+    }
+};
+// clang-format on
 
 TEST(RingBuf, CreateReturnsBufReturnedFromGetInstBuf)
 {
-    void *get_inst_buf_user_data = (void *)0x20;
-    mock()
-        .expectOneCall("mock_ring_buf_get_inst_buf")
-        .withParameter("user_data", get_inst_buf_user_data)
-        .andReturnValue((void *)&inst_buf);
-
-    RingBuf ring_buf;
-    RingBufInitCfg cfg = {
-        .get_inst_buf = mock_ring_buf_get_inst_buf,
-        .get_inst_buf_user_data = get_inst_buf_user_data,
-    };
-    uint8_t rc = ring_buf_create(&ring_buf, &cfg);
+    uint8_t rc = ring_buf_create(&ring_buf, &init_cfg);
 
     CHECK_EQUAL(RING_BUF_RESULT_CODE_OK, rc);
     CHECK_EQUAL((void *)&inst_buf, (void *)ring_buf);
